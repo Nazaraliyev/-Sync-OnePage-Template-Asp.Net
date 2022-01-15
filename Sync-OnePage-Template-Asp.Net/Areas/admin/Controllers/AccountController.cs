b475099103d0281bfb2d850266b32abdf789657d
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2022-01-15 14:09:25 +0400

    Creader Admin Area

---
+++ Sync-OnePage-Template-Asp.Net/Areas/admin/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sync_OnePage_Template_Asp.Net.Areas.admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
