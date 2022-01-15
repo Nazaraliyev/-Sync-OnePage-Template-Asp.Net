7e239802a6463c3793e75776e0e0e2ee29af8715
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2022-01-15 13:48:49 +0400

    Created All Model

---
+++ Sync-OnePage-Template-Asp.Net/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Sync_OnePage_Template_Asp.Net.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sync_OnePage_Template_Asp.Net.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Sync-OnePage-Template-Asp.Net/Models/CostumeUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sync_OnePage_Template_Asp.Net.Models
+{
+    public class CostumeUser:IdentityUser
+    {
+        [MaxLength(50), Required]
+        public string Name { get; set; }
+
+
+        [MaxLength(50), Required]
+        public string Lastname { get; set; }
+
+
+        [MaxLength(100), Required]
+        public string Fullname { get; set; }
+
+    }
+}
