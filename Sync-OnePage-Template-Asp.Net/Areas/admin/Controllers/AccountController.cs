8f90903e1035700de11d4afb3d0ebd2c1800e00e
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2022-01-16 20:09:24 +0400

    User Delete Is ready

--- Sync-OnePage-Template-Asp.Net/Areas/admin/Controllers/AccountController.cs
+++ Sync-OnePage-Template-Asp.Net/Areas/admin/Controllers/AccountController.cs
@@ -2,8 +2,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Sync_OnePage_Template_Asp.Net.Data;
+using Sync_OnePage_Template_Asp.Net.Models;
 using Sync_OnePage_Template_Asp.Net.ViewModel;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Sync_OnePage_Template_Asp.Net.Areas.admin.Controllers
 {
@@ -12,11 +15,13 @@ namespace Sync_OnePage_Template_Asp.Net.Areas.admin.Controllers
     {
         private readonly AppDbContext _context;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public AccountController(AppDbContext context, RoleManager<IdentityRole> roleManager)
+        public AccountController(AppDbContext context, RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
         {
             _context = context;
             _roleManager = roleManager;
+            _userManager = userManager;
         }
         public IActionResult Index()
         {
@@ -36,9 +41,46 @@ namespace Sync_OnePage_Template_Asp.Net.Areas.admin.Controllers
         }
 
 
+        public async Task<IActionResult> Delete(string Id)
+        {
+            if(Id != null)
+            {
+                if(_context.costumeUsers.Any(u => u.Id == Id)
+                {
+                    CostumeUser user = _context.costumeUsers.Find(Id);
+                    if(user.Profile != "progile.png")
+                    {
+                        string oldProfile = Path.Combine("wwwroot", "assets/img/profile", user.Profile);
+                        if (System.IO.File.Exists(oldProfile))
+                        {
+                            System.IO.File.Delete(oldProfile);
+                        }
+                    }
+
+                    await _userManager.DeleteAsync(user);
+                    return RedirectToAction(nameof(Index));
+
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+
+
+
         public IActionResult Login()
         {
             return View();
         }
+
+
+
     }
 }
